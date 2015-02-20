e2f714f0b32f3bbe3fba24e19109117488e423f9
Yu <yuwang@utexas.edu>
2015-02-19 23:21:08 -0600

    fix bugs in union, etc

--- CCS/xbee/XBEE_Sampler_TM4C/main.c
+++ CCS/xbee/XBEE_Sampler_TM4C/main.c
@@ -30,11 +30,11 @@ char static buf[BUFFER_SIZE];
 #if FRAME_STYLE == NICK_STYLE
 
 typedef union frame_u {
-	struct name {
+	struct {
 		char upper_four_bits;
 		char odd_byte;
 		char even_byte;
-	};
+	} name;
 
 	char array[3];
 } frame;
@@ -47,25 +47,25 @@ typedef struct tuple_s {
 frame encode_frame(const uint16_t *odd, const uint16_t *even) {
 	// magic: the TM4C architecture is Little endian and byte addressable
 	// this allows to take the bytes of the sample without incurring shift
-	uint8_t *odd_seg = odd;
-	uint8_t *even_seg = even;
+	uint8_t *odd_seg = (uint8_t *) odd;
+	uint8_t *even_seg = (uint8_t *) even;
 
 	frame f;
-	f.odd_byte = *odd_seg;
-	f.even_byte = *even_seg;
-	f.upper_four_bits = (odd_seg[1] << 4) | even_seg[1];
+	f.name.odd_byte = *odd_seg;
+	f.name.even_byte = *even_seg;
+	f.name.upper_four_bits = (odd_seg[1] << 4) | even_seg[1];
 
 	return f;
 }
 
 tuple decode_frame(frame f) {
-	uint16_t upper_four_bits = f.upper_four_bits;
+	uint16_t upper_four_bits = f.name.upper_four_bits;
 	tuple t;
-	uint8_t *odd_seg = &t.odd;
-	uint8_t *even_seg = &t.even;
+	uint8_t *odd_seg = (uint8_t *) &t.odd;
+	uint8_t *even_seg = (uint8_t *) &t.even;
 
-	*odd_seg = f.odd_byte;
-	*even_seg = f.even_byte;
+	*odd_seg = f.name.odd_byte;
+	*even_seg = f.name.even_byte;
 	odd_seg[1] = upper_four_bits >> 4;
 	even_seg[1] = upper_four_bits & 0xF;
 
